382332d53b066802a8ee40c58c2a659d34e67cd4
ygabuga <165772377+ygabuga@users.noreply.github.com>
2026-05-12 16:02:24 +0700

    Update run.py

--- run.py
+++ run.py
@@ -3,7 +3,6 @@ import uvicorn
 from app.database import init_db
 
 async def main():
-    await init_db()
     uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
 
 if __name__ == "__main__":
